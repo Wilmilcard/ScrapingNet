3ecb1e74d057643e5551174ca437a4eea8cd2ad7
juan leon <leonjuandavid@hotmail.com>
2024-10-04 16:40:51 -0500

    Agregar archivos de proyecto.

---
+++ ScrapingNet/Program.cs
@@ -0,0 +1,31 @@
+﻿using PuppeteerSharp;
+
+string url = "https://listado.mercadolibre.com.co/memorias-usb#D[A:memorias%20usb]";
+string chrome = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+
+var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+
+await using var browser = await Puppeteer.LaunchAsync(
+    new LaunchOptions
+    {
+        Headless = true,
+        ExecutablePath = chrome
+    });
+
+await using var page = await browser.NewPageAsync();
+
+await page.GoToAsync(url);
+
+var titles = new List<string>();
+var result = await page.EvaluateFunctionAsync<string[]>("()=>{" +
+    "const a = document.querySelectorAll('h2.poly-box.poly-component__title');" +
+    "const res = [];" +
+    "for(let i = 0; i < a.length; i++)" +
+    "   res.push(a[i].innerText);" +
+    "return res;" +
+    "}");
+
+foreach (var e in result)
+    Console.WriteLine(e);
+
