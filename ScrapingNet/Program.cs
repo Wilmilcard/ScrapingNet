aaa9464b24a7665bce105990bbebc5ab3d23bfb1
juan leon <leonjuandavid@hotmail.com>
2025-08-18 16:33:46 -0500

    ciclo repetitivo

--- ScrapingNet/Program.cs
+++ ScrapingNet/Program.cs
@@ -1,50 +1,65 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
 
 string url = "https://listado.mercadolibre.com.co/memorias-usb#D[A:memorias%20usb]";
 
 using HttpClient client = new HttpClient();
-string html = await client.GetStringAsync(url);
 
-HtmlDocument doc = new HtmlDocument();
-doc.LoadHtml(html);
-
-var productos = doc.DocumentNode.SelectNodes("//li[contains(@class, 'ui-search-layout__item')]");
-
-var listaProductos = new List<(string Nombre, decimal Precio)>();
-
-if (productos != null)
+bool salir = false;
+while (true)
 {
-    foreach (var producto in productos)
+    // Esperar 1 hora (3600000 ms)
+    await Task.Delay(TimeSpan.FromSeconds(5));
+
+    string html = await client.GetStringAsync(url);
+
+    HtmlDocument doc = new HtmlDocument();
+    doc.LoadHtml(html);
+
+    var productos = doc.DocumentNode.SelectNodes("//li[contains(@class, 'ui-search-layout__item')]");
+
+    var listaProductos = new List<(string Nombre, decimal Precio)>();
+
+    if (productos != null)
     {
-        var nombreNode = producto.SelectSingleNode(".//h3[contains(@class,'poly-component__title-wrapper')]/a");
-        string nombre = nombreNode?.InnerText.Trim() ?? "Sin nombre";
-
-        var precioNode = producto.SelectSingleNode(".//div[contains(@class,'poly-price__current')]//span[contains(@class,'andes-money-amount__fraction')]");
-        string precioTexto = precioNode?.InnerText.Trim().Replace(".", "") ?? "0";
-
-        if (decimal.TryParse(precioTexto, out decimal precio))
+        foreach (var producto in productos)
         {
-            listaProductos.Add((nombre, precio));
+            var nombreNode = producto.SelectSingleNode(".//h3[contains(@class,'poly-component__title-wrapper')]/a");
+            string nombre = nombreNode?.InnerText.Trim() ?? "Sin nombre";
+
+            var precioNode = producto.SelectSingleNode(".//div[contains(@class,'poly-price__current')]//span[contains(@class,'andes-money-amount__fraction')]");
+            string precioTexto = precioNode?.InnerText.Trim().Replace(".", "") ?? "0";
+
+            if (decimal.TryParse(precioTexto, out decimal precio))
+            {
+                listaProductos.Add((nombre, precio));
+            }
+
+            //Console.WriteLine($"Nombre: {nombre}");
+            //Console.WriteLine($"Precio: {precioTexto}");
+            //Console.WriteLine(new string('-', 40));
         }
 
-        Console.WriteLine($"Nombre: {nombre}");
-        Console.WriteLine($"Precio: {precioTexto}");
-        Console.WriteLine(new string('-', 40));
+        if (listaProductos.Any())
+        {
+            var masBarato = listaProductos.Where(x => x.Precio > 0).OrderBy(p => p.Precio).First();
+            
+            if(masBarato.Precio <= 5000)
+            {
+                Console.WriteLine($"Producto más barato:\n\n{masBarato.Nombre}\nPrecio: ${masBarato.Precio:N0}");
+                Console.WriteLine("\nEste es el producto mas barato ¿desea seguir buscando? (S/N)");
+                var key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.N)
+                {
+                    salir = true;
+                    break;
+                }
+            }
+        }
     }
-
-    if (listaProductos.Any())
+    else
     {
-        var masBarato = listaProductos.Where(x => x.Precio > 0).OrderBy(p => p.Precio).First();
-        Console.WriteLine($"Producto más barato:\n\n{masBarato.Nombre}\nPrecio: ${masBarato.Precio:N0}");
-        Console.ReadKey();
+        Console.WriteLine("No se encontraron productos.");
     }
-}
-else
-{
-    Console.WriteLine("No se encontraron productos.");
 }
 
 
